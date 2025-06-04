ba59c4f995060b8129c969f8c9aec6d4bfc49c4e
saltukbayar <138158011+saltuk1@users.noreply.github.com>
2025-06-04 10:11:54 +0200

    use of log return for CLT to apply

--- volatility.py
+++ volatility.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import numpy as np
 import math
 
-class Volatilty():
+class Volatility():
 
     def __init__(self, ticker : str, open : pd.Series, close : pd.Series , high : pd.Series , low : pd.Series):
-
         # Arguments passed as pd.Series
 
         self.ticker = ticker
@@ -15,7 +15,13 @@ class Volatilty():
         self.mean = self.close.mean()
         self.tradingdays = len(self.close)
     
-    def calculate_std_deviation(self):
-        return self.close.std()
+    def calculate_std_deviation(self, window = 20, annualizefactor = 252):
+        
+        logreturns = np.log(self.close / self.close.shift(1))
+        daily_vol = logreturns.rolling(window=window).std()
+        annualized_vol = daily_vol * np.sqrt(annualizefactor)
+        return annualized_vol
+
+
 
     
