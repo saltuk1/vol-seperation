a86fafe2ed9427296ed2813a41455689d5e559d3
saltukbayar <138158011+saltuk1@users.noreply.github.com>
2025-06-04 14:17:58 +0200

    parkinson volatility implemented

--- volatility.py
+++ volatility.py
@@ -4,7 +4,7 @@ import math
 
 class Volatility():
 
-    def __init__(self, ticker : str, open : pd.Series, close : pd.Series , high : pd.Series , low : pd.Series):
+    def __init__(self, ticker : str, open : pd.Series, close : pd.Series , high : pd.Series , low : pd.Series, window:int, annualizefactor:int):
         # Arguments passed as pd.Series
 
         self.ticker = ticker
@@ -12,15 +12,25 @@ class Volatility():
         self.close = close
         self.high = high
         self.low = low
-        self.mean = self.close.mean()
         self.tradingdays = len(self.close)
+        self.window = window
+        self.annualizefactor = annualizefactor
     
-    def calculate_std_deviation(self, window = 20, annualizefactor = 252):
+    def calculate_std_deviation(self):
         
         logreturns = np.log(self.close / self.close.shift(1))
-        daily_vol = logreturns.rolling(window=window).std()
-        annualized_vol = daily_vol * np.sqrt(annualizefactor)
-        return annualized_vol
+        daily_vol = logreturns.rolling(window=self.window).std()
+        annualized_std = daily_vol * np.sqrt(self.annualizefactor)
+        return annualized_std
+
+    def calculate_vol_parkinson(self):
+
+        high_low_ratio = np.log(self.high / self.low)
+        parkinson_factor = 1 / (4 * np.log(2))
+        parkinson_vol = np.sqrt(parkinson_factor * high_low_ratio**2)
+        annualized_vol_par = parkinson_vol * np.sqrt(self.annualizefactor)
+        return annualized_vol_par
+
 
 
 
