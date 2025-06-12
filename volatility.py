de1a88e58dfea6dd7a9494be0291280d6c46cce8
saltukbayar <138158011+saltuk1@users.noreply.github.com>
2025-06-12 20:34:09 +0200

    implemented yang zheng volatility

--- volatility.py
+++ volatility.py
@@ -10,6 +10,43 @@ class Volatility:
         self.data = data.copy()
         self.volatility_metrics = {}
         # self.signals = {} # Saving it for the future
+
+
+    def calculate_volatility_metrics(self, window: int = 30) -> Dict[str, pd.Series]:
+
+        # Calculate log returns
+        self.data['log_returns'] = np.log(self.data['Close']/self.data['close'].shift(1))
+
+        # Calculate volatility (Yang-Zheng)
+        self.volatility_metrics['Vol_YZ'] = self._yang_zheng_volatility(window)
+
+        # Calculate volatility (Parkinson)
+
+        # Calculate volatility (Garman-Klaas)
+
+        # Calculate volatility (Standard Deviation)
+
+    def _yang_zheng_volatility(self, window: int) -> pd.Series:
+
+        log_co = np.log(self.data['Close']/self.data['Open'])
+        log_ho = np.log(self.data['High']/self.data['Open'])
+        log_hc = np.log(self.data['High']/self.data['Close'])
+        log_lo = np.log(self.data['Low']/self.data['Open'])
+        log_lc = np.log(self.data['Low']/self.data['Close'])
+
+        log_oc_prev = np.log(self.data['Open']/self.data['Close'].shift(1))
+
+        k = 0.34 / (1.34 + ((window + 1)/window -1))
+
+        opentoclose_var = log_oc_prev.rolling(window).var()
+        closetoopen_var = log_co.rolling(window).var()
+        roger_satchell_var = ((log_hc*log_ho) + (log_lc*log_lo)).rolling(window).mean()
+
+        daily_yang_zheng_vol = np.sqrt(opentoclose_var + k*closetoopen_var + (1- k)*roger_satchell_var)
+        annualized_yang_zheng_vol = daily_yang_zheng_vol * np.sqrt(252)
+
+        return daily_yang_zheng_vol, annualized_yang_zheng_vol
+    
     
 '''    def calculate_std_deviation(self):
         
